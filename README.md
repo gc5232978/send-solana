0624299369413e8160ae64d3a40ed9850e3dc719
gc5232978 <gc5232978@gmail.com>
2024-06-14 15:08:21 -0600

    commit-1

---
+++ README.md
@@ -0,0 +1,19 @@
+# send-sol
+Sends Sol to a Public Key using Rust.
+
+## Description
+
+This project expands on the example found in the solana cookbook. 
+It shows how to use your Private Key to send Sol to a Public Key.
+```
+https://solanacookbook.com/references/basic-transactions.html
+```
+### Dependencies
+
+* solana-client = "1.18.16"
+* solana-program = "1.18.16"
+* solana-sdk = "1.18.16"
+
+### Notes
+
+Make sure to provide the correct path to your Private Key json file on line 12.
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::str::FromStr;
+use solana_client::rpc_client::RpcClient;
+use solana_program::system_instruction;
+use solana_sdk::commitment_config::CommitmentConfig;
+use solana_sdk::pubkey::Pubkey;
+use solana_sdk::signer::EncodableKey;
+use solana_sdk::transaction::Transaction;
+use solana_sdk::signature::{Keypair, Signer};
+
+fn main() {
+   
+    let from = Keypair::read_from_file("from.json")
+        .expect("Failed to load keypair");
+    let frompubkey = Signer::pubkey(&from);
+    println!("FROM: {}", frompubkey);
+   
+    let topubkey = Pubkey::from_str("3S7aRRYec4QtbnGLdzanAHvKrZaWmQB4LR6N2MNpyK3Q")
+        .expect("Failed to parse receivers pubkey");
+    println!("TO: {}", topubkey);
+
+    let lamports_to_send = 1_000_000; 
+    
+    let rpc_url = String::from(
+        "https://nd-161-276-093.p2pify.com/0d9c9b305572908d89d8388e0e558880"
+    );
+
+    let connection = RpcClient::new_with_commitment(
+        rpc_url, CommitmentConfig::confirmed()
+    );
+    
+    let ix = system_instruction::transfer(
+        &frompubkey,
+        &topubkey,
+        lamports_to_send,
+    );
+
+    let recent_blockhash = connection.get_latest_blockhash()
+        .expect("Failed to get recent blockhash.");
+
+    let txn = Transaction::new_signed_with_payer(
+        &[ix],
+        Some(&frompubkey),
+        &[&from],
+        recent_blockhash,
+    );
+
+    match connection.send_and_confirm_transaction(&txn) {
+        Ok(sig) => loop {
+            if let Ok(confirmed) = connection.confirm_transaction(&sig) {
+                if confirmed {
+                    println!("TX: {}", sig);
+                    println!("SUCCESS: {}", confirmed.to_string().to_uppercase());
+                    break;
+                }    
+            }
+        },
+        Err(e) => println!("ERROR: {}", e)
+    }
+}
